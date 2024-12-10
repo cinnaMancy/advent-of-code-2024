617d554149109529fabe6a1a35051c93dff31fea
Negyesi Nandor <nandor.negyesi@navitasoft.com>
2024-12-10 16:44:14 +0100

    debug code removed

--- src/main/kotlin/day10/Domain.kt
+++ src/main/kotlin/day10/Domain.kt
@@ -19,7 +19,7 @@ class HikingMap(
     private fun trails(tile: CharacterBoard.Tile): List<List<CharacterBoard.Tile>> = trails(listOf(tile), 0)
 
     private fun trails(path: List<CharacterBoard.Tile>, height: Int): List<List<CharacterBoard.Tile>> =
-        if (!path.last().content.isDigit() || height != path.last().content.digitToInt()) emptyList()
+        if (height != path.last().content.digitToInt()) emptyList()
         else if (height == 9) listOf(path)
         else heights.directlyAdjacent(path.last().coords).flatMap { trails(path.plus(it), height + 1) }
 
