cf1a52753db44cd4eaae6940934cbd13f9b4e0dc
Negyesi Nandor <nandor.negyesi@navitasoft.com>
2024-12-11 15:29:20 +0100

    day 11 works

--- src/main/kotlin/day11/Domain.kt
+++ src/main/kotlin/day11/Domain.kt
@@ -3,10 +3,10 @@ package day11
 class StoneLine(
     private val stones: List<Long>
 ) {
-    fun stonesCountAfterEvolutions(times: Int): Int = stones.sumOf { evolve(it, times).size }
+    fun stonesCountAfterEvolutions(times: Int): Long = stones.sumOf { sizeAfterEvolutions(it, times) }
 
-    private fun evolve(stone: Long, times: Int): List<Long> {
-        if (times == 0) return listOf(stone)
+    private fun sizeAfterEvolutions(stone: Long, times: Int): Long {
+        if (times == 0) return 1
 
         val memo = evolutionsMemo[Pair(stone, times)]
         if (memo != null) return memo
@@ -17,13 +17,13 @@ class StoneLine(
                 stone.toString().chunked(stone.toString().length / 2).map(String::toLong)
             else listOf(2024 * stone)
 
-        val nextResults = nextElements.flatMap { evolve(it, times - 1) }
+        val nextResults = nextElements.sumOf { sizeAfterEvolutions(it, times - 1) }
         evolutionsMemo[Pair(stone, times)] = nextResults
         return nextResults
     }
 
     companion object {
-        val evolutionsMemo = mutableMapOf<Pair<Long, Int>, List<Long>>()
+        val evolutionsMemo = mutableMapOf<Pair<Long, Int>, Long>()
 
         fun parse(text: String): StoneLine = StoneLine(text.split(" ").map(String::toLong))
     }
